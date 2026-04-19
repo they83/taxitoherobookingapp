89bbc0a3a13d1faf7657000694c6a65196f09bae
they83 <they83@yahoo.com>
2026-04-19 13:41:00 +0200

    To be tested fully (price check is up to 800km, text may need to be updated)

--- config/postgresql.js
+++ config/postgresql.js
@@ -1,6 +1,6 @@
 const postgresStatement =
-    `ALTER TABLE bookings ALTER COLUMN luggage SET NOT NULL;
-ALTER TABLE bookings ALTER COLUMN flight_nr SET NOT NULL;
+    `ALTER TABLE public.bookings ALTER COLUMN luggage SET NOT NULL;
+ALTER TABLE public.bookings ALTER COLUMN flight_nr SET NOT NULL;
 commit;
 `;
 
